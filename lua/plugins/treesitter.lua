c915ae979dad5ca4818ae3a27a0db920dc581d37
epicsagas <wowinno@gmail.com>
2026-06-01 14:49:11 +0900

    fix(nvim-treesitter): rewrite config for main branch API breaking changes
    - nvim-treesitter.configs module removed in main branch
    - highlight/indent now handled by Neovim 0.12 native treesitter
    - incremental_selection removed (no longer in main branch)
    - parser install uses new install.install() API

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,34 +4,34 @@ return {
     branch = "main",
     build = ":TSUpdate",
     config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "rust", "go", "lua", "vim", "vimdoc", "markdown", "toml", "yaml", "json",
-          "python", "c", "cpp", "java", "cmake", "make",
-          "javascript", "typescript", "tsx", "jsdoc",
-          "php", "phpdoc", "html", "css", "scss",
-          "sql", "kotlin", "dart",
-          "ruby", "commonlisp", "scheme", "r",
-          "c_sharp", "swift", "bash", "zig", "elixir", "haskell", "scala", "nim"
-        },
-        auto_install = true,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        indent = {
-          enable = true,
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<C-space>",
-            node_incremental = "<C-space>",
-            scope_incremental = false,
-            node_decremental = "<bs>",
-          },
-        },
-      })
+      require("nvim-treesitter").setup()
+
+      -- Ensure parsers are installed
+      local ensure_installed = {
+        "rust", "go", "lua", "vim", "vimdoc", "markdown", "toml", "yaml", "json",
+        "python", "c", "cpp", "java", "cmake", "make",
+        "javascript", "typescript", "tsx", "jsdoc",
+        "php", "phpdoc", "html", "css", "scss",
+        "sql", "kotlin", "dart",
+        "ruby", "commonlisp", "scheme", "r",
+        "c_sharp", "swift", "bash", "zig", "elixir", "haskell", "scala", "nim",
+      }
+
+      local parsers = require("nvim-treesitter.parsers")
+      local config = require("nvim-treesitter.config")
+      local install = require("nvim-treesitter.install")
+      local installed = config.get_installed()
+
+      local missing = vim.tbl_filter(function(lang)
+        return not vim.list_contains(installed, lang) and parsers[lang] ~= nil
+      end, ensure_installed)
+
+      if #missing > 0 then
+        install.install(missing)
+      end
+
+      -- Register markdown parser for Avante filetype
+      vim.treesitter.language.register("markdown", "Avante")
     end,
   },
 }
